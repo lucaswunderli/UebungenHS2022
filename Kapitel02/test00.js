34c0dc26feff1b8c9d5e7a43514a04f289efa0c1
lucaswunderli <114218330+lucaswunderli@users.noreply.github.com>
2022-09-30 12:22:25 +0200

    UebungenHS2022
    test

---
+++ Kapitel02/test00.js
@@ -0,0 +1,15 @@
+// Kommemtar
+// erstes javascript programm
+// (c) 2022 by lwu
+/* mehrzeiliger
+kommentar
+    eingerückt */
+console.log("Hello World"); //anweisung mit semikolon abschliessen, kiene formatierung nötig
+
+var a = 7;
+const pi = 3.1415926;
+
+
+a = 8;
+// pi = 22/7; #geht nicht, pi = konstant
+console.log(a,pi);
