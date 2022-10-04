5545eb39cb734abb12f7accd1988fe67f8a7a45b
lucaswunderli <114218330+lucaswunderli@users.noreply.github.com>
2022-10-04 13:06:43 +0200

    UebungenHS2022
    Hausaufgabe

---
+++ Kapitel02/Uebung02/Aufgabe1.js
@@ -0,0 +1,11 @@
+//Lucas WUnderli
+//Kapitel 2; Aufgabe 1
+
+var x = -1
+var Li = []
+
+for (var i;Li.length < 50;) {
+   Li.push(x =x+2);
+}
+
+console.log(Li);
---
+++ Kapitel02/Uebung02/Aufgabe2.js
@@ -0,0 +1,9 @@
+//Lucas WUnderli
+//Kapitel 2; Aufgabe 2
+
+function wurf(){
+    return(Math.floor(Math.random()*6+1));
+   
+    }
+ 
+console.log(wurf())
